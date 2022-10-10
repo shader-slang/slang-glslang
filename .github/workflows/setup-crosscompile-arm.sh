87e69f085921930963505f745e1f4bfe2a1a7f9b
Yong He <yonghe@outlook.com>
2022-10-10 14:02:18 -0700

    Fix crosscompile setup script.

--- .github/workflows/setup-crosscompile-arm.sh
+++ .github/workflows/setup-crosscompile-arm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update
 dpkg --status g++-9-aarch64-linux-gnu 2>&1 | grep -qP "not installed|not-installed"
 if [ $? -eq 0 ]; then
     sudo apt-get install -y g++-9-aarch64-linux-gnu
